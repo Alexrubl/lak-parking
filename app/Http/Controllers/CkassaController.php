2b0341f3573896f0387094b419dc07684f4c97fb
alexrubl@mail.ru <alexrubl@mail.ru>
2023-11-10 18:04:58 +0900

    permission

--- app/Http/Controllers/CkassaController.php
+++ app/Http/Controllers/CkassaController.php
@@ -134,6 +134,7 @@ class CkassaController extends Controller
         $response = curl_exec($curl);
         $info = curl_getinfo($curl);
         curl_close($curl);
+        info($response);
         return $response;
     }
 
