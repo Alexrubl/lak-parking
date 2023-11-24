47a3db336b3291f8de6547fcfe4d78ee336e903d
alexrubl@mail.ru <alexrubl@mail.ru>
2023-11-25 00:05:20 +0900

    fix entry

---
+++ app/Nova/Filters/TenantFilter.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Nova\Filters;
+
+use Laravel\Nova\Filters\Filter;
+use App\Models\Tenant;
+use Laravel\Nova\Http\Requests\NovaRequest;
+
+class TenantFilter extends Filter
+{
+    /**
+     * The filter's component.
+     *
+     * @var string
+     */
+    public $component = 'select-filter';
+
+    /**
+     * The displayable name of the filter.
+     *
+     * @var string
+     */
+    public $name = 'Арендаторы';
+
+    /**
+     * Apply the filter to the given query.
+     *
+     * @param  \Laravel\Nova\Http\Requests\NovaRequest  $request
+     * @param  \Illuminate\Database\Eloquent\Builder  $query
+     * @param  mixed  $value
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function apply(NovaRequest $request, $query, $value)
+    {
+        return $query->where('tenant_id', $value);
+    }
+
+    /**
+     * Get the filter's available options.
+     *
+     * @param  \Laravel\Nova\Http\Requests\NovaRequest  $request
+     * @return array
+     */
+    public function options(NovaRequest $request)
+    {
+        $options = array();        
+        foreach (Tenant::all('name', 'id') as $value) {
+            $options[$value->name] = $value->id;
+        }        
+        return $options;
+    }
+}
