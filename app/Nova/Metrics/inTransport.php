8a29385bb0ddb36180b840beec8c53641a6c7615
alexrubl@mail.ru <alexrubl@mail.ru>
2024-09-07 10:26:32 +0900

    fix jobs

--- app/Nova/Metrics/inTransport.php
+++ app/Nova/Metrics/inTransport.php
@@ -30,7 +30,7 @@ class inTransport extends Value
     {
         $timezone = Nova::resolveUserTimezone($request) ?? $request->timezone;      
         //return $this->result(DB::table('transports')->whereBetween( 'created_at', $this->currentRange($request->range, $timezone))->count());
-        return $this->count($request, \Auth::user()->isTenant() ? Transport::inside()->withTenant() : Transport::inside(), null, 'updated_at')->suffix('шт.');
+        return $this->count($request, \Auth::user()->isTenant() ? Transport::withTenant()->inside() : Transport::inside(), null, 'updated_at')->suffix('шт.');
     }
 
     /**
@@ -61,7 +61,7 @@ class inTransport extends Value
      */
     public function cacheFor()
     {
-       // return now()->addMinutes(5);
+       //return now()->addMinutes(5);
     }
 
     /**
